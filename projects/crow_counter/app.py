2014c9b09dcf908a68c7edf1cc74c9406311a001
PIKVICWORK\pikvic <truepikvic@gmail.com>
2019-05-22 13:17:52 +1000

    Added templates and add_crow functionality via submit form

--- projects/crow_counter/app.py
+++ projects/crow_counter/app.py
@@ -1,10 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
+crows_count = 0
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', count=crows_count)
+
+@app.route('/add_crow', methods=['POST'])
+def add_crow():
+    global crows_count
+    crows_count += 1  
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run(debug=True)
